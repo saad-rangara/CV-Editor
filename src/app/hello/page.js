772c68722ed0673ddbc62c01b077426b8cb59698
yusuf mahmood <yusufmahmood00@gmail.com>
2024-11-12 13:49:19 +0000

    layout and css update UI

---
+++ src/app/hello/page.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+export default function About() {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="https://gdoc.io/uploads/job-winning-cv-free-google-docs-template-t.webp"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">About</h1>
+          <p className="py-6">
+            CV maker was born in 2024 due to demand for a CV maker that beats
+            all cv makers
+          </p>
+          <Link href="/resume">
+            <button className="btn btn-primary">Get Started</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
